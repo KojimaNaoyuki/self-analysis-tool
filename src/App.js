014bc7c2dc2df3298364a689923f354a392cb784
KojimaNaoyuki <programkoji213@gmail.com>
2021-12-24 17:18:14 +0900

    ステータスバー開発完了

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+import StatusBar from './component/presentational/atoms/StatusBar';
+
+const BaseBox = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #EFEFEF;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BaseBox>
+      <StatusBar step1Color="#8DCFFF" step2Color="#FFF" step3Color="#FFF"/>
+    </BaseBox>
   );
 }
 
---
+++ src/component/presentational/atoms/StatusBar.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+const StatusBarBox = styled.div`
+    position: relative;
+    top: 20px;
+    left: 30px;
+    width: 70%;
+    height: 8px;
+    background-color: #FFF;
+
+    z-index: 0;
+`;
+
+const StatusBar = (props) => {
+    const StatusBarCircle1 = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: ${props.step1Color};
+    border-radius: 50%;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform translate(-50%, -25%);
+
+    z-index: 100;
+    `
+    const StatusBarBox1 = styled.div`
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 50%;
+    height: 8px;
+    background-color: ${props.step2Color};
+    z-index: 10;
+    `;
+    const StatusBarCircle2 = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: ${props.step2Color};
+    border-radius: 50%;
+
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform translate(-50%, -25%);
+
+    z-index: 100;
+    `
+    const StatusBarBox2 = styled.div`
+    position: absolute;
+    top: 0px;
+    left: 50%;
+    width: 50%;
+    height: 8px;
+    background-color: ${props.step3Color};
+    z-index: 10;
+    `;
+    const StatusBarCircle3 = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color:  ${props.step3Color};
+    border-radius: 50%;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform translate(50%, -25%);
+
+    z-index: 100;
+`
+
+    return(
+        <StatusBarBox>
+            <StatusBarCircle1 />
+            <StatusBarBox1 />
+            <StatusBarCircle2 />
+            <StatusBarBox2 />
+            <StatusBarCircle3 />
+        </StatusBarBox>
+    );
+}
+export default StatusBar;
