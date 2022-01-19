c6ad74fc544ef5641729383ac50539773c009eeb
KojimaNaoyuki <programkoji213@gmail.com>
2022-01-20 01:41:08 +0900

    途中経過

--- src/component/container/ManagementPage.js
+++ src/component/container/ManagementPage.js
@@ -7,9 +7,36 @@ import Header from "../presentational/organisms/Header";
 const ManagementPageBox = styled.div`
 `;
 
-const SelectJobBox = styled.div``;
+const InputBox = styled.div`
+    background-color: #FFF;
+`;
 
-const SelectJobInput = styled.select``;
+const Flex = styled.div`
+    display: flex;
+`;
+const SubTitle = styled.h2`
+    padding: 8px 0 8px 15px;
+    width: 40%;
+    margin: 0;
+    font-size: 20px;
+    background-color: #8DCFFF;
+`;
+const Content = styled.div`
+    padding: 8px 0;
+    width: 60%;
+    text-align: center;
+`;
+const SelectJobInput = styled.select`
+    width: 90%;
+    text-align: center;
+    border: none;
+    outline: none;
+`;
+const Input = styled.input`
+    border: solid 1px #7e7e7e;
+    border-radius: 3px;
+    outline: none;
+`;
 
 class ManagementPage extends Component {
     constructor() {
@@ -23,12 +50,25 @@ class ManagementPage extends Component {
                 {/* ヘッダー */}
 
                 {/* 管理対象職種選択 */}
-                <SelectJobBox>
-                    <SelectJobInput>
-                        <option>総合職</option>
-                    </SelectJobInput>
-                </SelectJobBox>
+                <InputBox>
+                    <Flex>
+                        <SubTitle>管理対象</SubTitle>
+                        <Content>
+                            <SelectJobInput>
+                                <option>総合職</option>
+                                <option>エンジニア(一般)</option>
+                                <option>エンジニア(制作物)</option>
+                            </SelectJobInput>
+                        </Content>
+                    </Flex>
+                </InputBox>
                 {/* 管理対象職種選択 */}
+
+                {/* 質問一覧 */}
+                {/* 質問一覧 */}
+
+                {/* 質問追加 */}
+                {/* 質問追加 */}
 
             </ManagementPageBox>
         );
