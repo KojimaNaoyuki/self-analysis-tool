56081aba2b1e54796903e9f1949ccb5199901080
KojimaNaoyuki <programkoji213@gmail.com>
2022-01-20 12:10:14 +0900

    途中経過

--- src/component/container/ManagementPage.js
+++ src/component/container/ManagementPage.js
@@ -1,8 +1,10 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import styled from 'styled-components';
+import QuestionList from "../presentational/atoms/QuestionList";
 
 import Header from "../presentational/organisms/Header";
+import arrowImg from "../../img/arrow.svg";
 
 const ManagementPageBox = styled.div`
 `;
@@ -38,6 +40,27 @@ const Input = styled.input`
     outline: none;
 `;
 
+const HeadingTitle = styled.h3`
+    margin: 0;
+    position relative;
+    padding: 10px 60px;
+    font-size: 20px;
+`;
+const ArrowImg = styled.img`
+    position absolute;
+    top: 50%;
+    left: 20px;
+    width: 22px;
+    transform: translateY(-50%);
+`;
+
+const MtbL = styled.div`
+    margin: 60px 0;
+`;
+const MtbS = styled.div`
+    margin: 5px 0;
+`;
+
 class ManagementPage extends Component {
     constructor() {
         super();
@@ -48,6 +71,8 @@ class ManagementPage extends Component {
                 {/* ヘッダー */}
                 <Header />
                 {/* ヘッダー */}
+
+                <MtbL />
 
                 {/* 管理対象職種選択 */}
                 <InputBox>
@@ -64,10 +89,22 @@ class ManagementPage extends Component {
                 </InputBox>
                 {/* 管理対象職種選択 */}
 
+                <MtbL />
+
                 {/* 質問一覧 */}
+                <InputBox>
+                    <HeadingTitle><ArrowImg src={arrowImg} />質問一覧</HeadingTitle>
+                    <QuestionList id="0" Qtext="hogehoge" />
+                    <QuestionList id="1" Qtext="hogehoge" />
+                </InputBox>
                 {/* 質問一覧 */}
 
+                <MtbS />
+
                 {/* 質問追加 */}
+                <InputBox>
+                    <HeadingTitle><ArrowImg src={arrowImg} />質問を追加</HeadingTitle>
+                </InputBox>
                 {/* 質問追加 */}
 
             </ManagementPageBox>
